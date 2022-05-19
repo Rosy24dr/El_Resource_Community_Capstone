13ee0378815f503961e1cf253ac72f007add3fed
Rossebell <rosy24dr@gmail.com>
2022-05-19 12:07:11 -0400

    created comment model, registered the model with admin site and migrated new comment table

--- backend/comment/models.py
+++ backend/comment/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from authentication.models import User
 
-# Create your models here.
+class Comment(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    forum_id = models.CharField(max_length=400)
+    text = models.CharField(max_length=600)
+    date = models.IntegerField()
+    
